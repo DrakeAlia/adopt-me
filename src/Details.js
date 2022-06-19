63c3896026b01a2170edc62578cbe1f26bd69911
Drake Alia <52472741+DrakeAlia@users.noreply.github.com>
2022-06-19 11:25:06 -0700

    fetching data in componentDidMount

--- src/Details.js
+++ src/Details.js
@@ -8,9 +8,18 @@ class Details extends Component {
       this.state = { loading: true };
    }
 
+   async componentDidMount() {
+      const res = await fetch(
+         `http://pets-v2.dev-apis.com/pets?id=${this.props.params.id}`
+      );
+      const json = await res.json();
+
+      this.setState({ loading: false, ...json.pets[0] });
+   }
+
    render() {
       if (this.state.loading) {
-         return <h2>loading..... </h2>
+         return <h2>loading.....</h2>
       }
 
       const { animal, breed, city, state, description, name } = this.state;
@@ -26,8 +35,13 @@ class Details extends Component {
                <p>{description}</p>
             </div>
          </div>
-      )
+      );
    }
 }
 
-export default Details;
+const WrappedDetails = () => {
+   const params = useParams();
+   return <Details params={params} />;
+};
+
+export default WrappedDetails;
