559690aa8459a34a84dc2195cdb8282d25bec618
Drake Alia <52472741+DrakeAlia@users.noreply.github.com>
2022-06-19 10:54:11 -0700

    changed functional component into class component for details.js file

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import { render } from "react-dom";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import SearchParams from "./SearchParams";
 import { StrictMode } from "react";
-import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import Details from "./Details";
 
 const App = () => {
